359a6dcc1b074710eb9b6bd426a11d68a0194c55
Wordllban <mininkov3228@gmail.com>
2025-01-17 18:26:02 +0200

    setup houdini

---
+++ houdini.config.js
@@ -0,0 +1,14 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "watchSchema": {
+        "url": "https://countries.trevorblades.com/graphql"
+    },
+    "runtimeDir": ".houdini",
+    "plugins": {
+        "houdini-svelte": {}
+    }
+}
+
+export default config
